a902ad768fa18efbaf3f8a2a5bf6c2e808db9952
Laura Antunez <lauraantunez024@gmail.com>
2022-05-30 18:56:07 -0400

    Finished creating test and classes files

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,44 @@
 const Intern = require("../lib/Intern")
+
+describe('Intern', () =>{
+    describe("initialization", () => {
+
+        it("should set school using the constructor arguments", () => {
+            // Arrange
+            const school = "Harvard";
+            // Act
+            const intern = new Intern("Aku", 666, "aku@etest.com", school); 
+            // Assert,
+            expect(intern.school).toBe(school);
+
+        });
+
+        
+    });
+
+    describe("Get Method", () => {
+
+        it("should get school using getSchool()", () => {
+            // Arrange
+            const school = "Harvard";
+            // Act
+            const intern = new Intern("Aku", 666, "aku@test.com", school); 
+            const internSchool = intern.getSchool();
+            // Assert
+            expect(internSchool).toBe(school);
+        });
+
+        it("should get Intern role using getRole()", () => {
+            // Arrange
+            const role = "Intern";
+            // Act
+            const intern = new Intern ("Aku", 666, "aku@test.com", "Harvard"); 
+            const internRole = intern.getRole();
+            // Assert
+            expect(internRole).toBe(role);
+        });
+
+
+    });
+
+})
