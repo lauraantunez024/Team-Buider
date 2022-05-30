bda87e49bcc8e482c010e924e18d5142ad3522cc
Laura Antunez <lauraantunez024@gmail.com>
2022-05-30 18:29:47 -0400

    finished employee js, finished manager test js, started manager js

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,38 @@
+const Manager = require("../lib/Manager")
+
+describe('Manager', () =>{
+    describe("initialization", () => {
+
+        it("should set office number using the constructor arguments", () => {
+            // Arrange
+            const officeNumber = 4444;
+            // Act
+            const manager = new Manager("Aku", 666, "aku@etest.com", officeNumber); 
+            // Assert,
+            expect(manager.officeNumber).toBe(officeNumber);
+
+        });
+
+        
+    });
+
+    describe("Get Method", () => {
+
+        it("should get manager role using getRole()", () => {
+            // Arrange
+            const role = "Manager";
+            // Act
+            const manager = new Manager("Aku", 666, "aku@test.com", 4444); 
+            const managerRole = manager.getRole();
+            // Assert
+            expect(managerRole).toBe(role);
+        });
+
+
+    });
+
+
+
+})
+
+ 
